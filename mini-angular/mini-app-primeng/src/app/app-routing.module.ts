bc62d8126672086360fc32f30dbc5cdc56a5e0d7
truongphucuong <truongphucuong95vn@gmail.com>
2019-01-19 20:08:06 +0700

    2019-01-19: update primeng

--- mini-angular/mini-app-primeng/src/app/app-routing.module.ts
+++ mini-angular/mini-app-primeng/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InputPrimengComponent } from './myComponent/input-primeng/input-primeng.component';
+const routes: Routes = [
+  {path: "input/first" , component: InputPrimengComponent }
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  declarations:[
+    InputPrimengComponent
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- mini-angular/mini-app-primeng/src/app/app.module.ts
+++ mini-angular/mini-app-primeng/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppInputPrimeNGRoutingModule } from './myComponent/input-primeng/input-primeng-routing.module';
 import { InputTextModule } from 'primeng/inputtext';
+import { DashboardComponent } from './myComponent/dashboard/dashboard.component';
+import { HeroesComponent } from './myComponent/heroes/heroes.component';
+import { HeroDetailComponent } from './myComponent/hero-detail/hero-detail.component';
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AppInputPrimeNGRoutingModule,
-    InputTextModule
+    InputTextModule,
+    FormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
